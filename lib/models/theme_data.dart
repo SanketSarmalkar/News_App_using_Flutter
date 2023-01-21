bacb3bd696ad5d803892b6b2147f78ed0610882f
SanketSarmalkar <sanket.sarmalkar2002@gmail.com>
2023-01-21 23:03:18 +0530

    model for themecolors

---
+++ lib/models/theme_data.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class ThemeColorData {
+
+  final List<Color> lightColor =[
+    Colors.black,
+    Colors.white24,
+    Colors.white38,
+    Colors.white60,
+    Colors.white,
+  ];
+  final List<Color> darkColor =[
+    Colors.white,
+    Colors.black12,
+    Colors.black26,
+    Colors.black38,
+    Colors.black,
+  ];
+
+}

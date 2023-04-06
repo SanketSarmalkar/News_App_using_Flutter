17847411aaec49a368a744e697c9dfd55b5cbdaa
SanketSarmalkar <sanket.sarmalkar2002@gmail.com>
2023-04-06 23:53:59 +0530

    update

--- lib/services/remote_services.dart
+++ lib/services/remote_services.dart
@@ -1,11 +1,13 @@
 import 'package:http/http.dart' as http;
 import 'package:news_app/models/articles.dart';
+import 'package:news_app/keys/api_key.dart';
 
 class RemoteServices {
   static var client = http.Client();
+  APIKey apiKey = APIKey();
   static Future<Welcome?>? fetchNews(var country, var category) async {
     var response = await client.get(Uri.parse(
-        'https://newsapi.org/v2/top-headlines?country=${country.toString()}${"&category=$category"}&apiKey=01970ce6240f47bf97b0a62aa35d70f3'));
+        'https://newsapi.org/v2/top-headlines?country=${country.toString()}${"&category=$category"}&apiKey=${APIKey.newsApiKey}'));
     if (response.statusCode == 200) {
       var jsonString = response.body;
       return welcomeFromJson(jsonString);
